465c203917410e713913c7d9cee8b54d81c0603f
carlospenaforte <carlosedu_1132@hotmail.com>
2025-04-15 21:15:30 -0300

    fixed bugs in video game model/entities. get mediatr and make a handler

--- VSAvideogame/Entities/VideoGame.cs
+++ VSAvideogame/Entities/VideoGame.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Genre { get; set; } = string.Empty;
-        public string RealeaseYear { get; set; }
+        public int RealeaseYear { get; set; }
 
     }
 }
---
+++ VSAvideogame/Features/VideoGames/GetAllGames.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VSAvideogame.Data;
+using VSAvideogame.Entities;
+
+namespace VSAvideogame.Features.VideoGames
+{
+    public static class GetAllGames
+    {
+        public record Query : IRequest<IEnumerable<Response>>;
+
+        public record Response(int Id, string Title, string Genre, int RealeaseYear);
+
+        public class Handler(VideoGameDbContext context) : IRequestHandler<Query, IEnumerable<Response>>
+        {
+            public async Task<IEnumerable<Response>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var games = await context.VideoGames.ToListAsync(cancellationToken);
+                return games.Select(g => new Response(g.Id, g.Title, g.Genre, g.RealeaseYear));
+            }
+        }
+
+    }
+
+    [ApiController]
+
+}
